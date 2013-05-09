5e588667123313a87e56847a53b8174f163678ae
Aaron Trostle <aaron.trostle@gmail.com>
2013-05-08 18:56:22 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LunchTracker::Application.config.secret_token = 'c121f214f3173db2811a87cfebeff7924cd34d152e99389e3f234fee7f0fc4ef3b2e9841c8993065972f0bd059998bc351f47f6ba70ecf5f27d647b4a9855641'
